e675579e1793130f9cddf785c00d68e12e5da268
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 23:38:42 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+  i = 0
+  selection = []
+  while i < array.length
+    if yield(array[i])==true
+      selection << array[i]
+    end
+    i+=1
+  end
+  selection
 end

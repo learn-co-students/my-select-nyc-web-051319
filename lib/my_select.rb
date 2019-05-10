adc67c683a30ad5ad95f281a3fab9934f52c8908
Elma Rahman <elmarahman635@gmail.com>
2019-05-10 23:13:14 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  i = 0
+ name_collection = []
+  while i < collection.length
+    if yield(collection[i])
+  name_collection.push(collection[i])
 end
+      i += 1
+    end
+    name_collection
+end
+
+#if yield is t, add to array

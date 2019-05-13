11c95139c2b5199bd2ef4e868778c53fd2af31f8
Raphael Saphra <dsaphra@Macintosh-c82a1445a595.local>
2019-05-13 08:41:41 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
+def get_length(collection)
+  collection.length
+end
+
 def my_select(collection)
- # your code here!
+  new_collection = []
+  if block_given?
+    i = 0
+    collection_length = get_length(collection)
+    while i < collection_length
+      new_collection_value = yield(collection[i])
+      if new_collection_value
+        new_collection.push(collection[i])
+      end
+      i += 1
+    end
+  new_collection
+  else
+    collection
+  end
 end
